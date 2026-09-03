f6c9d62bf3836ea125e0e6da36e140322793559b
Bruna Villanova <114350625+brunavillanova@users.noreply.github.com>
2026-09-02 22:12:23 -0300

    Preparar backend para producao

--- backend/server.js
+++ backend/server.js
@@ -575,10 +575,8 @@ app.use(
 // SERVIDOR
 // =====================================================
 
-app.listen(PORT, () => {
-  console.log(
-    `Servidor rodando na porta ${PORT}`
-  );
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 
   console.log(
     "Banco de dados conectado."
